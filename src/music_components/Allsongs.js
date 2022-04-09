619929243c6df6aba9f59e87ef20149895629dc5
Swapnil <swapniljain240@gmail.com>
2022-04-09 17:36:42 +0530

    play screen

--- src/music_components/Allsongs.js
+++ src/music_components/Allsongs.js
@@ -1,13 +1,26 @@
 import db from '../firebase'
 
+const handleSongClick=()=>{
 
+}
 const allSongs = db
 .collection('')
 .get();
+
 const Allsongs = (props)=>{
     return (
         <div className ='music_component'>
-           
+           {
+               allSongs.map(
+                   (song)=>{
+                       return(
+                           <div onClick={this.handleSongClick}>
+                                <h2>song.name</h2>
+                           </div>
+                       )
+                   }
+               )
+           }
         </div>
     )
 }
