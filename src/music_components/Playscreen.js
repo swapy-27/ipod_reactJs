dec8690668b315259bbe90d533c07fc1ab3f07da
Swapnil <swapniljain240@gmail.com>
2022-04-11 13:32:40 +0530

    PlayScreen

--- src/music_components/Playscreen.js
+++ src/music_components/Playscreen.js
@@ -1,12 +1,15 @@
-const Playscreen = () =>{
+const Playscreen = (props) =>{
+    // const curr_song = props.state.song[props.state.curr_playing_song]
     return (
         <div className = 'play_screen_container'>
-            <div className='song_name'></div>
-
+            <div className='song_name'>
+                {/* <h2>{curr_song.name}</h2> */}
+            </div>
+                
             <div className=''>
 
                 <div className='song_image'>
-
+                {/* <img src={curr_song.name} alt='song'/> */}
                 </div>
                 
                 <div className='audio_tab'>
@@ -14,7 +17,8 @@ const Playscreen = () =>{
                 </div>
 
             </div>
-            <div><h2>press button to pause/play</h2></div>
+            <div><h2>press <span><i class="fa-solid fa-play"></i>
+                <i class="fa-solid fa-pause"></i></span> button to pause/play</h2></div>
         </div>
     )
 }
